060f9bb6f433634e7b80bbb247ace715cc9bf0d9
dhruvlakhani <dhruvlakhani198@gmail.com>
2022-08-30 23:20:33 +0530

    Practical-2

---
+++ app/src/main/java/com/example/madpractical2_20012011055/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.madpractical2_20012011055
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    val TAG ="MainActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        showMessage("onCreate called")
+    }
+    fun showMessage(msg:String){
+        Log.i(TAG,msg)
+        Toast.makeText(this,msg, Toast.LENGTH_SHORT).show()
+        Snackbar.make(findViewById(R.id.snackbarDisplay),msg,Snackbar.LENGTH_SHORT).show()
+    }
+    override fun onStart() {
+        showMessage("onStart called")
+        super.onStart()
+    }
+    override fun onRestart() {
+        showMessage("onRestart called")
+        super.onRestart()
+    }
+    override fun onDestroy() {
+        showMessage("onDestroy called")
+        super.onDestroy()
+    }
+    override fun onStop() {
+        showMessage("onStop called")
+        super.onStop()
+    }
+    override fun onResume() {
+        showMessage("onResume called")
+        super.onResume()
+    }
+}
